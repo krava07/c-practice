ecd6c24d32afe2e44a9093a0d4f6203b514f6784
krava07 <krava07@gmail.com>
2016-04-12 20:11:23 +0300

    rot13.c added

---
+++ rot13.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int c,d;
+
+    while((c=getchar())!=EOF)
+    {
+        if (c>='A' && c<='Z') {
+            if ((d=c+13) <= 'Z') {
+                putchar(d);
+            } else {
+                d=c-13;
+                putchar(d);
+            }
+        }
+        else 
+        if (c>='a' && c<='z') {
+            if ((d=c+13) <= 'z') {
+                putchar(d);
+            } else {
+                d=c-13;
+                putchar(d);
+            }
+        else {
+            putchar(c);
+        }
+        }
+    }
+
+return 0;
+}
