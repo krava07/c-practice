701149c09321e925865121b1ce896649670a81e0
krava07 <krava07@gmail.com>
2016-04-27 17:42:07 +0300

    upd

--- dyn_arr.cpp
+++ dyn_arr.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
 
 class DynArr
 {
@@ -10,18 +14,41 @@ public:
         arr = new int [size + 1];
     };
     DynArr(DynArr& x) {
+        delete [] arr;
         size = x.size;
         arr = new int [size];
     }
+    void Add(int x) {
+        int *temp = new int [size + 1];
+        for(int i = 0; i < size; ++i) {
+            temp[i] = arr[i];
+        }
+        
+        delete [] arr;
+        arr = temp;
+ 
+        arr[size] = x;
+        ++size;
+    }
+    void Print() {
+        for (int i = 0; i < size; i++) {
+            cout<<arr[i]<<" ";
+        }
+    }
     ~DynArr() {
         delete []arr;
-    };
-         
+    };      
 };
 
 
 int main()
 {
-    DynArr a[5];
+    DynArr a;
+    a.Add(5);
+    a.Add(4);
+    a.Add(3);
+    a.Add(2);
+    
+    a.Print();
     return 0;
 }
